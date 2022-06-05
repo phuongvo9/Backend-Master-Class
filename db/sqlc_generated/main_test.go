4dd65f3b6b2d3bdbba74ae109ba8a2180572f035
phuongvo9 <huyphuong4116@gmail.com>
2022-06-05 22:07:59 +0700

    Test: Unit tests for CreateAccount func

--- db/sqlc_generated/main_test.go
+++ db/sqlc_generated/main_test.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"database/sql"
+	// " _ " keep the import package not to be removed when `go mod tidy`
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
@@ -13,16 +15,18 @@ import (
 var testQueries *Queries
 
 const (
+	//need to install Postgres driver for Golang => use go get github.com/lib/pq
 	dbDrive  = "postgres"
 	dbSource = "postgresql://postgres:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
-// TestMain will be the entry point for executing all unit-tests in a same GOlang package
-func TestMain(t *testing.M) {
+// TestMain with para m will be the entry point for executing all unit-tests in a same GOlang package
+
+func TestMain(m *testing.M) {
 	// use sql.Open to open a database Connection
 	conn, err := sql.Open(dbDrive, dbSource)
 	if err != nil {
-		log.Fatalln("can not connect to the database")
+		log.Fatal("can not connect to the database")
 	}
 	// we use the connection to create a new Queries object
 	testQueries = New(conn)
