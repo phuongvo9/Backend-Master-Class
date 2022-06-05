b04d7ed4c943c459703ca592091f7a002c597a42
phuongvo9 <huyphuong4116@gmail.com>
2022-06-05 21:45:06 +0700

    define: TestMain for entrypoint of unit tests

--- db/sqlc_generated/main_test.go
+++ db/sqlc_generated/main_test.go
@@ -1,6 +1,32 @@
 package db
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+)
+
 // All unit tests will need a object "Queries" => declare as a global variable
-// {Queries struct} contans DBTX which can be a database connection or a database transaction.
+// {Queries struct} contains DBTX which can be a database connection or a database transaction.
 // In these test cases, we will use testQueries as a db connection and use it to create a Queries object
 var testQueries *Queries
+
+const (
+	dbDrive  = "postgres"
+	dbSource = "postgresql://postgres:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+// TestMain will be the entry point for executing all unit-tests in a same GOlang package
+func TestMain(t *testing.M) {
+	// use sql.Open to open a database Connection
+	conn, err := sql.Open(dbDrive, dbSource)
+	if err != nil {
+		log.Fatalln("can not connect to the database")
+	}
+	// we use the connection to create a new Queries object
+	testQueries = New(conn)
+
+	// use m.Run() to start running the Unit tests and return correct exit code by os.Exit()
+	os.Exit(m.Run())
+}
